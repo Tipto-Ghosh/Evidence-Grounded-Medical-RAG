0382f07153c6c1804a27af509b387495fc877907
Tipto-Ghosh <tiptoghosh@gmail.com>
2026-03-23 21:28:19 +0600

    Setup and requirements added.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "medChat",
+    version = "0.1.0",
+    author = "Tipto-Ghosh",
+    author_email = "tiptoghosh@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
